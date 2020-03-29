74dfba5897b0969c573298b857c4aad3e7ea6007
Carolina <caro.negrelli@gmail.com>
2020-03-29 19:57:22 -0400

    Package prepeared to be uploaded to pypi

--- lessons/ObjectOrientedProgramming/IdeFiles/5_exercise_upload_to_pypi/setup.py
+++ lessons/ObjectOrientedProgramming/IdeFiles/5_exercise_upload_to_pypi/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='distributionsGB',
       version='0.1',
-      description='Gaussian distributions',
-      packages=['distributions'],
+      description='Gaussian and binomial distributions',
+      packages=['distributionsGB'],
+      author = 'Carolina Negrelli',
+      author_email = 'caro.negrelli@gmail.com',
       zip_safe=False)
